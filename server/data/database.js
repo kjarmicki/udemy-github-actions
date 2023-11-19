8c70318d47ba6a0dd311d25c6bb12a3d9d1e9cd7
Krystian Jarmicki <jarmicki@gmail.com>
2023-11-19 18:57:45 +0100

    log connection uri

--- server/data/database.js
+++ server/data/database.js
@@ -6,6 +6,7 @@ const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+console.log('connection uri', uri);
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
